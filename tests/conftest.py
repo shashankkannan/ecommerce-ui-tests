9d7e6216c597e34dd6d8824abb33e3b2cf61e6a0
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2025-06-10 19:34:43 -0400

    conf

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,39 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless=new")  # or comment out for visible mode
-    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--start-maximized")
-    options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-    )
 
-    driver = webdriver.Chrome(options=options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
+
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# import pytest
+
+# @pytest.fixture
+# def driver():
+#     options = Options()
+#     options.add_argument("--headless=new")  # or comment out for visible mode
+#     options.add_argument("--disable-gpu")
+#     options.add_argument("--no-sandbox")
+#     options.add_argument("--window-size=1920,1080")
+#     options.add_argument("--start-maximized")
+#     options.add_argument(
+#         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+#     )
+
+#     driver = webdriver.Chrome(options=options)
+#     yield driver
+#     driver.quit()
